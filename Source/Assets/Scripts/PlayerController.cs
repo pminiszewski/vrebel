7300a6939bc1f0a85fc974dd399aa8b50499994a
EF <venetian5@gmail.com>
2014-05-15 19:16:44 +0200

    - Added Thruster particle system to character - Added Thruster control in PlayerController

--- Source/Assets/Scripts/PlayerController.cs
+++ Source/Assets/Scripts/PlayerController.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+    private Character _Character;
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+    void Awake()
+    {
+        _Character = GameplayStatics.Character;
+    }
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+        float thrust = Input.GetAxis("Fire1");
+        if (thrust >= 0)
+        {
+            _Character.Thrust(thrust);
+        }
+            
+        
+
 	}
+
+
 }
