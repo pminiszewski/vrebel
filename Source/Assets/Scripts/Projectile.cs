6ae5714e063e673644bf6da6ec56d141ee2e9ef5
EF <venetian5@gmail.com>
2014-05-16 01:03:35 +0200

    - Added projectile base class - Added projectile bullet - Added damage dealing to enemy characters - Added projectile material - Added enemy Grunt prefab

---
+++ Source/Assets/Scripts/Projectile.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour 
+{
+
+    public float LifeSpan = 4;
+    public float Speed = 10;
+    public float Damage = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision colision)
+    {
+        Character c = colision.collider.GetComponent<Character>();
+        if (c != null)
+        {
+            c.OnHit(this);
+        }
+        Destroy(gameObject);
+    }
+
+    IEnumerator BulletTimeout()
+    {
+        while (true)
+        {
+
+            yield return new WaitForSeconds(LifeSpan);
+            Destroy(gameObject);
+        }
+
+    }
+}
