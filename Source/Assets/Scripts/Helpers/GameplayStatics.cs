bdffba3cb777871c6f4b1a2f9e7454e0cae10bea
EF <venetian5@gmail.com>
2014-05-15 19:11:26 +0200

    - Added gameplayStatics class

---
+++ Source/Assets/Scripts/Helpers/GameplayStatics.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Class that holds most important references to game objects easily accessible.
+/// </summary>
+public static class GameplayStatics
+{
+    private static T Get<T>(string tag) where T : MonoBehaviour
+    {
+        GameObject go = GameObject.FindGameObjectWithTag("Character");
+        if (go == null)
+        {
+            Debug.LogError("No object with tag Character in the scene.");
+        }
+        T comp = go.GetComponent<T>();
+        if (comp == null)
+        {
+            Debug.LogError("Character object has no Character component.");
+        }
+        return comp;
+    }
+
+    public static Lazy<Character> Character = new Lazy<Character>(() => { return Get<Character>("Character"); });
+    public static Lazy<GameController> GameController = new Lazy<GameController>(() => { return Get<GameController>("GameController"); });
+    public static Lazy<PlayerController> PlayerController = new Lazy<PlayerController>(() => { return Get<PlayerController>("Player"); });
+
+
+}
